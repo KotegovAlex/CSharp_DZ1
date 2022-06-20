7445c74fb3efc2875fad709a3b0bf863ba7d9288
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-06-20 10:49:42 +0500

    add Task1 max number

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a >= b) 
+{
+    Console.WriteLine($"Максимальное значение {a}");
+}
+else
+{
+    Console.WriteLine($"Максимальное значение {b}");
+}
