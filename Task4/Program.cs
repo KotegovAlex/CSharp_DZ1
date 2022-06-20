14414d7a9462a668ef169198122a091a64a024f8
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-06-20 12:50:15 +0500

    add Task4 even numbers from 1 to N

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int count = 2;
+
+while (count < N + 1)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+        count = count + 2;
+    }
+}
+if (N == 0 || N == 1) Console.WriteLine($"Для числа N = {N} нет четных чисел в диапазоне от 1 до N");
